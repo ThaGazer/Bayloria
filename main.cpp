a56b7ebc7d707739317a253ac238df25aac21891
ThaGazer <hi5ritter@hotmail.com>
2014-10-23 15:46:37 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/*
+  Author:         Justin Ritter, Cole Crawford, Nannan
+  Title:          Bayloria
+  Descriptions:   Text base game. There are 4 main lvls to the map. 
+                  In each lvl there is a boss and once you defeat the boss
+                  you get a special key. There are 4 other keys hidden on the lvl 
+                  that are need to get intot the boss room. At the 4th lvl 
+                  all 20 room keys are need to fight the ultimate boss. 
+                  Once you defeat him you win the game.
