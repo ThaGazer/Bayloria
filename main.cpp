445e998bcb662c519c4f2baabdb5eadba0c78083
ThaGazer <hi5ritter@hotmail.com>
2014-10-24 13:23:05 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,67 @@ using namespace std;
 
 int main()
 {
+  int locMain = 1;
+  int locRoom = 1;
+  int keys = 0;
   
+  while(keys < 5)
+  {
+    switch(locMain)
+    {
+      case 1:;
+        cout << "Map loc 1";
+        while(locRoom < 6)
+        {
+          switch(locRoom)
+          {
+            case 1:;
+              cout << "Room 1";
+              locRoom++;
+            break; //locRoom 1
+            
+            case 2:;
+              cout << "Room 2";
+              locRoom++;
+            break; //locRoom 2
+            
+            case 3:;
+              cout << "Room 3";
+              locRoom++;
+            break; //locRoom 3
+            
+            case 4:;
+              cout << "Room 4";
+              locRoom++;
+            break; //locRoom 4
+            
+            case 5:;
+              cout << "Room 5";
+              locRoom++;
+            break; //locRoom 5
+          } //swtich locRoom
+        } //while locRoom
+        locMain++;
+      break; //locMain 1
+      
+      case 2:;
+        cout << "Map loc 2";
+        locMain++;
+      
+      break; //case 2
+      
+      case 3:;
+        cout << "Map loc 3";
+        locMain++;
+      break; //case 3
+      
+      case 4:;
+        cout << "Map loc 4";
+        locMain++;
+      break; //case 4
+      
+    }
+  }
   
   
   
