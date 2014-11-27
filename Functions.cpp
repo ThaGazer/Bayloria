1d19febacc6d2452dc66aebde5705507d4c74bd7
clcrwfrd <coleluvsku@gmail.com>
2014-11-27 01:37:57 -0600

    Update Functions.cpp
    I worked a lot on the inventory today, also worked a tiny bit on attack function... I'm gonna try to finish inventory when I get the names of all the items and all that shit.

--- Functions.cpp
+++ Functions.cpp
@@ -14,9 +14,11 @@
 #include <iostream>
 #include "Functions.h"
 
+using namespace std;
+
 Entity::Entity()
 {
-  name = "Bob The Slayer";
+  name = "Justin Crawford";
   attackDam = 0;
   health = 0;
   armor = 0;
@@ -51,12 +53,12 @@ double Entity::getSpeed()
   return speed;
 }
 
-std::string Entity::getName()
+string Entity::getName()
 {
   return name;
 }
 
-void Entity::setName(std::string nam)
+void Entity::setName(string nam)
 {
   name = nam;
 }
@@ -80,11 +82,6 @@ void Entity::setArmor(int arm)
   armor = arm;
 }
 
-int Entity::attack()
-{
-  return 0;
-}
-
 Player::Player()
 {
   setName("Bob The Slayer");
@@ -92,20 +89,69 @@ Player::Player()
   setHealth(0);
   setArmor(0);
   setSpeed(0);
+
+	// initializes the arrays to false for the bool and empty space for the strings
+	for (int i = 0; i < INVENTORY_MAX; i++)
+	{
+		inventory[i] = false;
+	}
+
+	//inventoryString[INVENTORY_MAX] = {"sword", 
 }
 
-Player::Player(int att, int heal, int arm, double spe, std::string nam )
+Player::Player(int att, int heal, int arm, double spe, string nam, bool inv[], string invStr[])
 {
   setName(nam);
   setAttack(att);
   setHealth(heal);
   setArmor(arm);
   setSpeed(spe);
+	setInventory(inv, invStr);
+}
+
+void Player::getInventory()
+{
+	for (int i = 0; i < INVENTORY_MAX; i++)
+	{
+		cout << inventory[i] << endl;
+	}
+
+	cout << "invstr" << endl;
+
+	for (int i = 0; i < INVENTORY_MAX; i++)
+	{
+		if (inventory[i] == true)
+		{
+			cout << inventoryString[i] << endl;
+		}
+	}
+}
+
+void Player::setInventory(bool inv[], string invStr[])
+{
+	for (int i = 0; i < INVENTORY_MAX; i++)
+	{
+		inventory[i] = inv[i];
+		inventoryString[i] = invStr[i];
+	}
+}
+
+void Player::addItemToInventory(int itemNumber)
+{
+	for (int i = 0; i < (itemNumber - 1); i++)
+	{
+		inventory[i] = true;
+		/*if (inventoryString[i] == "sword")
+		{
+			attackDam += 10;
+		}*/
+		//else if (inventory[i] == "
+	}
 }
 
 void instructions()
 {
-  std::cout <<  "\nThe point of the game is to defeat all four of the main "
+  cout <<  "\nThe point of the game is to defeat all four of the main "
               "bosses\n and progress through the 4 levels of Bayloria\n"
               "To play the game all you have to do is enter the four numbers\n"
               "that relate to the door you want to go through\n"
@@ -125,48 +171,48 @@ void loc1()
   int locRoom1 = 1;
   int direct = 0;
 
-  std::cout << "Map loc 1\n";
+  cout << "Map loc 1\n";
   while(locRoom1 < 6)
   {
     switch(locRoom1)
     {
       case 1:;
-        std::cout << "Room 1\n";
-        std::cout << "Which direction would you like to move?\n";
-        std::cin >> direct;
+        cout << "Room 1\n";
+        cout << "Which direction would you like to move?\n";
+        cin >> direct;
         while(direct != 4)
         {
-          std::cout << "That door is closed\n";
-          std::cin >> direct;
+          cout << "That door is closed\n";
+          cin >> direct;
         }
         if(direct == 4)
            locRoom1++;
       break; //locRoom1 1
 
       case 2:;
-        std::cout << "Room 2\n";
-        std::cout << "Which direction would you like to move?\n";
+        cout << "Room 2\n";
+        cout << "Which direction would you like to move?\n";
         while(direct != 4)
         {
-          std::cout << "That door is closed\n";
-          std::cin >> direct;
+          cout << "That door is closed\n";
+          cin >> direct;
         }
         if(direct == 4)
            locRoom1++;
       break; //locRoom1 2
 
       case 3:;
-        std::cout << "Room 3\n";
+        cout << "Room 3\n";
         locRoom1++;
       break; //locRoom1 3
 
       case 4:;
-        std::cout << "Room 4\n";
+        cout << "Room 4\n";
         locRoom1++;
       break; //locRoom1 4
 
       case 5:;
-        std::cout << "Room 5\n";
+        cout << "Room 5\n";
         locRoom1++;
       break; //locRoom1 5
     } //swtich locRoom1
@@ -178,48 +224,48 @@ void loc2()
   int locRoom2 = 1;
   int direct = 0;
 
-  std::cout << "Map loc 2\n";
+  cout << "Map loc 2\n";
   while(locRoom2 < 9)
   {
     switch(locRoom2)
     {
       case 1:;
-        std::cout << "Room 1\n";
+        cout << "Room 1\n";
         locRoom2++;
       break; //locRoom2 1
 
       case 2:;
-        std::cout << "Room 2\n";
+        cout << "Room 2\n";
         locRoom2++;
       break; //locRoom2 2
 
       case 3:;
-        std::cout << "Room 3\n";
+        cout << "Room 3\n";
         locRoom2++;
       break; //locRoom2 3
 
       case 4:;
-        std::cout << "Room 4\n";
+        cout << "Room 4\n";
         locRoom2++;
       break; //locRoom2 4
 
       case 5:;
-        std::cout << "Room 5\n";
+        cout << "Room 5\n";
         locRoom2++;
       break; //locRoom2 5
 
       case 6:;
-        std::cout << "Room 6\n";
+        cout << "Room 6\n";
         locRoom2++;
       break; //locRoom2 6
 
       case 7:;
-        std::cout << "Room 7\n";
+        cout << "Room 7\n";
         locRoom2++;
       break; //locRoom2 7
 
       case 8:;
-        std::cout << "Room 8\n";
+        cout << "Room 8\n";
         locRoom2++;
       break; //locRoom2 8
     } //swtich locRoom2
@@ -231,63 +277,63 @@ void loc3()
   int locRoom3 = 1;
   int direct = 0;
 
-  std::cout << "Map loc 3\n";
+  cout << "Map loc 3\n";
   while(locRoom3 < 12)
   {
     switch(locRoom3)
     {
       case 1:;
-        std::cout << "Room 1\n";
+        cout << "Room 1\n";
         locRoom3++;
       break; //locRoom3 1
 
       case 2:;
-        std::cout << "Room 2\n";
+        cout << "Room 2\n";
         locRoom3++;
       break; //locRoom3 2
 
       case 3:;
-        std::cout << "Room 3\n";
+        cout << "Room 3\n";
         locRoom3++;
       break; //locRoom3 3
 
       case 4:;
-        std::cout << "Room 4\n";
+        cout << "Room 4\n";
         locRoom3++;
       break; //locRoom3 4
 
       case 5:;
-        std::cout << "Room 5\n";
+        cout << "Room 5\n";
         locRoom3++;
       break; //locRoom3 5
 
       case 6:;
-        std::cout << "Room 6\n";
+        cout << "Room 6\n";
         locRoom3++;
       break; //locRoom3 6
 
       case 7:;
-        std::cout << "Room 7\n";
+        cout << "Room 7\n";
         locRoom3++;
       break; //locRoom3 7
 
       case 8:;
-        std::cout << "Room 8\n";
+        cout << "Room 8\n";
         locRoom3++;
       break; //locRoom3 8
 
       case 9:;
-        std::cout << "Room 9\n";
+        cout << "Room 9\n";
         locRoom3++;
       break; //locRoom3 9
 
       case 10:;
-        std::cout << "Room 10\n";
+        cout << "Room 10\n";
         locRoom3++;
       break; //locRoom3 10
 
       case 11:;
-        std::cout << "Room 11\n";
+        cout << "Room 11\n";
         locRoom3++;
       break; //locRoom3 11
     } //swtich locRoom3
@@ -299,80 +345,100 @@ void loc4()
   int locRoom4 = 1;
   int direct = 0;
 
-  std::cout << "Map loc 4\n";
+  cout << "Map loc 4\n";
   while(locRoom4 < 15)
   {
     switch(locRoom4)
     {
       case 1:;
-        std::cout << "Room 1\n";
+        cout << "Room 1\n";
         locRoom4++;
       break; //locRoom4 1
 
       case 2:;
-        std::cout << "Room 2\n";
+        cout << "Room 2\n";
         locRoom4++;
       break; //locRoom4 2
 
       case 3:;
-        std::cout << "Room 3\n";
+        cout << "Room 3\n";
         locRoom4++;
       break; //locRoom4 3
 
       case 4:;
-        std::cout << "Room 4\n";
+        cout << "Room 4\n";
         locRoom4++;
       break; //locRoom4 4
 
       case 5:;
-        std::cout << "Room 5\n";
+        cout << "Room 5\n";
         locRoom4++;
       break; //locRoom4 5
 
       case 6:;
-        std::cout << "Room 6\n";
+        cout << "Room 6\n";
         locRoom4++;
       break; //locRoom4 6
 
       case 7:;
-        std::cout << "Room 7\n";
+        cout << "Room 7\n";
         locRoom4++;
       break; //locRoom4 7
 
       case 8:;
-        std::cout << "Room 8\n";
+        cout << "Room 8\n";
         locRoom4++;
       break; //locRoom4 8
 
       case 9:;
-        std::cout << "Room 9\n";
+        cout << "Room 9\n";
         locRoom4++;
       break; //locRoom4 9
 
       case 10:;
-        std::cout << "Room 10\n";
+        cout << "Room 10\n";
         locRoom4++;
       break; //locRoom4 10
 
       case 11:;
-        std::cout << "Room 11\n";
+        cout << "Room 11\n";
         locRoom4++;
       break; //locRoom4 11
 
       case 12:;
-        std::cout << "Room 12\n";
+        cout << "Room 12\n";
         locRoom4++;
       break; //locRoom4 12
 
       case 13:;
-        std::cout << "Room 13\n";
+        cout << "Room 13\n";
         locRoom4++;
       break; //locRoom4 13
 
       case 14:;
-        std::cout << "Room 14\n";
+        cout << "Room 14\n";
         locRoom4++;
       break; //locRoom4 14
     } //swtich locRoom4
   } //while locRoom4
+}
+
+void attack(Player player, Entity entity)
+{
+	while(player.getHealth() > 0 && entity.getHealth() > 0)
+	{
+		int healthEnemy = entity.getHealth();
+		int yourHealth = player.getHealth();
+
+		if (player.getSpeed() > entity.getSpeed())
+		{
+			cout << "You attack first!" << endl;
+			entity.setHealth(healthEnemy -= player.getAttack() - entity.getArmor());
+		}
+		else
+		{
+			cout << "The enemy was quicker! It attacks you first!" << endl;
+			player.setHealth(yourHealth -= entity.getAttack() - player.getArmor());
+		}
+	}
 }
