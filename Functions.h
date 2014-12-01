b9a129770d6e6da432206b90771e23aaaa386602
clcrwfrd <coleluvsku@gmail.com>
2014-11-30 20:44:27 -0600

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -10,76 +10,31 @@
   Date Created:   10/23/2014
 */
 
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
 
-#include <string>
-
-using namespace std;
-
-class Entity
-{
-private:
-	int attackDam;
-  int health;
-  int armor;
-  double speed;
-  string name;
-
-public:
-  Entity();
-  Entity(int, int, int, double);
-  int getHealth();
-  int getAttack();
-  int getArmor();
-  double getSpeed();
-  string getName();
-  void setName(string);
-  void setSpeed(double);
-  void setHealth(int);
-  void setAttack(int);
-  void setArmor(int);
-};
-
-class Player : public Entity
-{
-private:
-	static const int INVENTORY_MAX = 25;
-	int gold;
-
-	bool inventory[INVENTORY_MAX];
-	string inventoryString[INVENTORY_MAX];
-	int inventoryStats[INVENTORY_MAX + 1];
-	string inventoryType[INVENTORY_MAX + 1];
-	int inventoryNum[INVENTORY_MAX];
-
-public:
-  Player();
-  Player(int, int, int, double, string, int, bool [], string [], int [], string [], int []);
-	int getGold();
-	void setGold(int);
-	void getInventory();
-	void setInventory(bool [], string [], int [], string [], int []);
-	void addItemToInventory(int);
-  
-};
+#include "Entity.h"
+#include "Player.h"
 
 void instructions();
-void loc1();
-void loc2();
-void loc3();
-void loc4();
+void whatToDo(int);
+void loc1(Player);
+void loc2(Player);
+void loc3(Player);
+void loc4(Player);
 
 void attack(Player, Entity);
-void store(Player);
+void store(Player &);
 
-void createWizard(Entity);
-void createKnight(Entity);
-void createArcher(Entity);
+void createWizard(Entity &);
+void createKnight(Entity &);
+void createArcher(Entity &);
 
-void createPrimusRatticus(Entity);
-void createSecondusSerpent(Entity);
-void createStagOfTertius(Entity);
-void createLordFarquaad(Entity);
+void createPrimusRatticus(Entity &);
+void createSecondusSerpent(Entity &);
+void createStagOfTertius(Entity &);
+void createLordFarquaad(Entity &);
+
+void openTheChest(bool, bool, Player, int &, bool &);
 
 #endif
